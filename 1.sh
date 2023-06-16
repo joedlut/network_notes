e3452d65cbab31a0d4e938fe1eb03146ca0ac8f8
root <2532771263@qq.com>
2023-06-17 00:38:39 +0800

    fix

--- 1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/joedlut/network_notes.git
-git push -u origin master
